53c3ea974284b95556116e1eabd864aef63642aa
LouJou <loutjie.joubert@gmail.com>
2019-05-21 22:28:14 +0200

    Delete ggl_analytics.php

--- ggl_analytics.php
+++
@@ -1,9 +0,0 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-XXXXXXXXX-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-XXXXXXXXX-1');
-</script>
