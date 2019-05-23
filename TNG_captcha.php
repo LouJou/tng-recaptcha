9fac5a9a43854bc63ab5f18fb168dcd4d444b373
LouJou <loutjie.joubert@gmail.com>
2019-05-23 20:27:29 +0200

    Added POST version
    Added POST version in addition to GET version as the POST is specified by Google and probably more secure.

--- TNG_captcha.php
+++ TNG_captcha.php
@@ -29,18 +29,36 @@ if($tngSiteKey && $tngSecret) {
 
 		$curl = curl_init();
 
-		curl_setopt_array($curl, array(
-		  CURLOPT_URL => "https://www.google.com/recaptcha/api/siteverify?secret=".$tngSecret."&response=".$_POST["g-recaptcha-response"]."&remoteip=".$remoteip,
-		  CURLOPT_RETURNTRANSFER => true,
-		  CURLOPT_ENCODING => "",
-		  CURLOPT_MAXREDIRS => 10,
-		  CURLOPT_TIMEOUT => 30,
-		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		  CURLOPT_CUSTOMREQUEST => "GET",
-		  CURLOPT_HTTPHEADER => array(
-		    "cache-control: no-cache",
-		  ),
-		));
+	    	curl_setopt_array($curl, array(
+	      	CURLOPT_URL => "https://www.google.com/recaptcha/api/siteverify",
+	      	CURLOPT_RETURNTRANSFER => true,
+	      	CURLOPT_ENCODING => "",
+	      	CURLOPT_MAXREDIRS => 10,
+	      	CURLOPT_TIMEOUT => 30,
+	      	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_POST => true,
+		CURLOPT_POSTFIELDS => array(
+			'secret' => $tngSecret,
+			'response' => $_POST["g-recaptcha-response"],
+			'remoteip' => $remoteip
+		),
+	      	CURLOPT_HTTPHEADER => array(
+			"cache-control: no-cache",
+	     	 ),
+	   	 ));
+	    	// GET VERION
+		// curl_setopt_array($curl, array(
+		//  CURLOPT_URL => "https://www.google.com/recaptcha/api/siteverify?secret=".$tngSecret."&response=".$_POST["g-recaptcha-response"]."&remoteip=".$remoteip,
+		//  CURLOPT_RETURNTRANSFER => true,
+		//  CURLOPT_ENCODING => "",
+		//  CURLOPT_MAXREDIRS => 10,
+		//  CURLOPT_TIMEOUT => 30,
+		//  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		//  CURLOPT_CUSTOMREQUEST => "GET",
+		//  CURLOPT_HTTPHEADER => array(
+		//    "cache-control: no-cache",
+		//  ),
+		// ));
 
 		$response = curl_exec($curl);
 		$err = curl_error($curl);
